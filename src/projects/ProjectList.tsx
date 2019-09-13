900c5f5f0b2db844e443498fd1dcbe98aded9adc
Craig McKeachie <craigdmckeachie@gmail.com>
2019-09-13 17:57:26 -0400

    lab10

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
+import ProjectForm from './ProjectForm';
 
 interface ProjectListProps {
   projects: Project[];
@@ -12,6 +13,7 @@ class ProjectList extends React.Component<ProjectListProps> {
     const items = projects.map(project => (
       <div key={project.id} className="cols-sm">
         <ProjectCard project={project} />
+        <ProjectForm/>
       </div>
     ));
     return <div className="row">{items}</div>;
