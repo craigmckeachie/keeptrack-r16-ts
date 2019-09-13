817f8cbc4e0a8543394aa9921315692603641583
Craig McKeachie <craigdmckeachie@gmail.com>
2019-09-13 15:29:01 -0400

    lab05

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { MOCK_PROJECTS } from './MockProjects';
 
 class ProjectsPage extends React.Component {
-  render(){
-    return <h1>Projects</h1>;
+  render() {
+    return (
+      <Fragment>
+        <h1>Projects</h1>
+        <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+      </Fragment>
+    );
   }
 }
 
