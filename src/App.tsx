adeec4bf81d6debd43cd2d98fea7a1740620895b
Craig McKeachie <craigdmckeachie@gmail.com>
2019-09-13 15:06:35 -0400

    lab04

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import ProjectsPage from './projects/ProjectsPage';
+
 
 const App: React.FC = () => {
   return (
-    <blockquote cite="Benjamin Franklin">
-      Tell me and I forget, teach me and I may remember, involve me and I learn.
-    </blockquote>
+    <div className="container">
+      <ProjectsPage />
+    </div>
   );
 };
 
