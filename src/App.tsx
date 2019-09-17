dc292943d93d67fa52e9fade457bbd4e0aff9990
Craig McKeachie <craigdmckeachie@gmail.com>
2019-09-17 16:33:50 -0400

    lab20

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import './App.css';
 import ProjectsPage from './projects/ProjectsPage';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from 'react-router-dom';
+import HomePage from './home/HomePage';
 
 const App: React.FC = () => {
   return (
-    <div className="container">
-      <ProjectsPage />
-    </div>
+    <Router>
+      <header className="sticky">
+        <a href="#" className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </a>
+        <NavLink to="/" exact className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects/" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/projects" exact component={ProjectsPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
--- src/projects/projectAPI.ts
+++ src/projects/projectAPI.ts
@@ -43,16 +43,17 @@ function delay(ms: number) {
 
 const projectAPI = {
   get(page = 1, limit = 20) {
-    return fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
-      .then(delay(600))
-      .then(checkStatus)
-      .then(parseJSON)
-      .catch((error: TypeError) => {
-        console.log('log client error ' + error);
-        throw new Error(
-          'There was an error retrieving the projects. Please try again.'
-        );
-      });
+    return (
+      fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
+        // .then(checkStatus)
+        .then(parseJSON)
+        .catch((error: TypeError) => {
+          console.log('log client error ' + error);
+          throw new Error(
+            'There was an error retrieving the projects. Please try again.'
+          );
+        })
+    );
   },
 
   put(project: Project) {
