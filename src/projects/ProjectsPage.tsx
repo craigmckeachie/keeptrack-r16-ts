87bbeacbc7aa342006b32d9b7711879af1cf3098
Craig McKeachie <craigdmckeachie@gmail.com>
2019-09-17 15:14:54 -0400

    lab18

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,16 +1,52 @@
 import React, { Fragment } from 'react';
-import { MOCK_PROJECTS } from './MockProjects';
 import ProjectList from './ProjectList';
 import { Project } from './Project';
+import { projectAPI } from './projectAPI';
 
 interface ProjectsPageState {
   projects: Project[];
+  loading: boolean;
+  error: string | undefined;
+  page: number;
 }
 
 class ProjectsPage extends React.Component<any, ProjectsPageState> {
   state = {
-    projects: MOCK_PROJECTS
+    projects: [],
+    loading: false,
+    error: undefined,
+    page: 1
   };
+
+  loadProjects(page: number) {
+    this.setState({ loading: true });
+    projectAPI
+      .get(page)
+      .then(data => {
+        if (page === 1) {
+          this.setState({ projects: data, loading: false, page });
+        } else {
+          this.setState(previousState => {
+            return {
+              projects: [...previousState.projects, ...data],
+              loading: false,
+              page
+            };
+          });
+        }
+      })
+      .catch(error => this.setState({ error: error.message, loading: false }));
+  }
+
+  componentDidMount() {
+    this.loadProjects(this.state.page);
+  }
+
+  handleMoreClick = () => {
+    const nextPage = this.state.page + 1;
+    this.loadProjects(nextPage);
+  };
+
   saveProject = (project: Project) => {
     this.setState((previousState: ProjectsPageState) => {
       let projects = previousState.projects.map((p: Project) => {
@@ -23,7 +59,42 @@ class ProjectsPage extends React.Component<any, ProjectsPageState> {
     return (
       <Fragment>
         <h1>Projects</h1>
+        {this.state.error && (
+          <div className="row">
+            <div className="card large error">
+              <section>
+                <p>
+                  <span className="icon-alert inverse "></span>
+                  {this.state.error}
+                </p>
+              </section>
+            </div>
+          </div>
+        )}
+
         <ProjectList onSave={this.saveProject} projects={this.state.projects} />
+
+        {!this.state.loading && !this.state.error && (
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="button-group fluid">
+                <button
+                  className="button default"
+                  onClick={this.handleMoreClick}
+                >
+                  More...
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {this.state.loading && (
+          <div className="center-page">
+            <span className="spinner primary"></span>
+            <p>Loading...</p>
+          </div>
+        )}
       </Fragment>
     );
   }
