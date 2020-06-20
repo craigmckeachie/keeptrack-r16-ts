f38910ee1bb2104015606a8b022f318ea339798a
Craig McKeachie <craigdmckeachie@gmail.com>
2020-06-20 16:26:56 -0400

    adding formatting for description

--- src/projects/ProjectCard.tsx
+++ src/projects/ProjectCard.tsx
@@ -12,6 +12,9 @@ function ProjectCard(props: ProjectCardProps) {
   const handleEditClick = (projectBeingEdited: Project) => {
     onEdit(projectBeingEdited);
   };
+  const formatDescription = (description: string): string => {
+    return description.substring(0, 60) + '...';
+  };
   return (
     <div className="card">
       <img src={project.imageUrl} alt={project.name} />
@@ -20,7 +23,7 @@ function ProjectCard(props: ProjectCardProps) {
           <h5 className="strong">
             <strong>{project.name}</strong>
           </h5>
-          <p>{project.description}</p>
+          <p>{formatDescription(project.description)}</p>
         </Link>
         <p>Budget : {project.budget.toLocaleString()}</p>
         <button
