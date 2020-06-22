f585e0505085e74208aceac0a38eeb552ed23bb3
Craig McKeachie <craigdmckeachie@gmail.com>
2020-06-22 16:36:31 -0400

    list now fc with hooks

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,61 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
 import ProjectForm from './ProjectForm';
 
-interface ProjectListProps {
+export interface ProjectListProps {
   projects: Project[];
   onSave: (project: Project) => void;
 }
 
-interface ProjectListState {
-  editingProject: Project | {};
-}
+const ProjectList: React.FC<ProjectListProps> = ({ projects, onSave }) => {
+  const [editingProject, setEditingProject] = useState({});
 
-class ProjectList extends React.Component<ProjectListProps, ProjectListState> {
-  state = {
-    editingProject: {}
-  };
-  handleEdit = (project: Project) => {
-    this.setState({ editingProject: project });
+  const handleEdit = (project: Project) => {
+    setEditingProject(project);
   };
 
-  cancelEditing = () => {
-    this.setState({ editingProject: {} });
+  const cancelEditing = () => {
+    setEditingProject({});
   };
 
-  render() {
-    const { projects, onSave } = this.props;
+  let item: JSX.Element;
+  const items = projects.map((project: Project) => {
+    if (project !== editingProject) {
+      item = (
+        <div key={project.id} className="cols-sm">
+          <ProjectCard
+            project={project}
+            onEdit={() => {
+              handleEdit(project);
+            }}
+          ></ProjectCard>
+        </div>
+      );
+    } else {
+      item = (
+        <div key={project.id} className="cols-sm">
+          <ProjectForm
+            project={project}
+            onSave={onSave}
+            onCancel={cancelEditing}
+          ></ProjectForm>
+        </div>
+      );
+    }
+    return item;
+  });
 
-    let item: JSX.Element;
-    const items = projects.map((project: Project) => {
-      if (project !== this.state.editingProject) {
-        item = (
-          <div key={project.id} className="cols-sm">
-            <ProjectCard
-              project={project}
-              onEdit={() => {
-                this.handleEdit(project);
-              }}
-            ></ProjectCard>
-          </div>
-        );
-      } else {
-        item = (
-          <div key={project.id} className="cols-sm">
-            <ProjectForm
-              project={project}
-              onSave={onSave}
-              onCancel={this.cancelEditing}
-            ></ProjectForm>
-          </div>
-        );
-      }
-      return item;
-    });
-
-    return <div className="row">{items}</div>;
-  }
-}
+  return <div className="row">{items}</div>;
+};
 
 export default ProjectList;
+
+// interface ProjectListState {
+//   editingProject: Project | {};
+// }
+
+// class ProjectList extends React.Component<ProjectListProps, ProjectListState> {
+//   state = {
+//     editingProject: {},
+//   };
+//   handleEdit = (project: Project) => {
+//     this.setState({ editingProject: project });
+//   };
+
+//   cancelEditing = () => {
+//     this.setState({ editingProject: {} });
+//   };
+
+//   render() {
+//     const { projects, onSave } = this.props;
+
+//     let item: JSX.Element;
+//     const items = projects.map((project: Project) => {
+//       if (project !== this.state.editingProject) {
+//         item = (
+//           <div key={project.id} className="cols-sm">
+//             <ProjectCard
+//               project={project}
+//               onEdit={() => {
+//                 this.handleEdit(project);
+//               }}
+//             ></ProjectCard>
+//           </div>
+//         );
+//       } else {
+//         item = (
+//           <div key={project.id} className="cols-sm">
+//             <ProjectForm
+//               project={project}
+//               onSave={onSave}
+//               onCancel={this.cancelEditing}
+//             ></ProjectForm>
+//           </div>
+//         );
+//       }
+//       return item;
+//     });
+
+//     return <div className="row">{items}</div>;
+//   }
+// }
+
+// export default ProjectList;
