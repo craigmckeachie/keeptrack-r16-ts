3515cdb1c4753a3a8fa556004211725846b3a80f
Craig McKeachie <craigdmckeachie@gmail.com>
2020-06-22 17:04:46 -0400

    projectpage is fc with hooks

--- src/projects/ProjectPage.tsx
+++ src/projects/ProjectPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { projectAPI } from './projectAPI';
 import ProjectDetail from './ProjectDetail';
 import { Project } from './Project';
@@ -9,50 +9,105 @@ interface ProjectPageState {
   error: string | undefined;
 }
 
-class ProjectPage extends React.Component<any, ProjectPageState> {
-  state = {
-    loading: false,
-    project: undefined,
-    error: ''
-  };
-
-  componentDidMount() {
-    const id = Number(this.props.match.params.id);
-    this.setState({ loading: true });
+const ProjectPage: React.FC<ProjectPageState> = (props: any) => {
+  const [loading, setLoading] = useState(false);
+  const [project, setProject] = useState(null);
+  const [error, setError] = useState(null);
+  const id = Number(props.match.params.id);
+  useEffect(() => {
+    setLoading(true);
     projectAPI
       .find(id)
-      .then(data => this.setState({ project: data, loading: false }))
-      .catch(e => this.setState({ error: e.message, loading: false }));
-  }
-  render() {
-    const { loading, project, error } = this.state;
-    return (
-      <React.Fragment>
-        <h1>Project Detail</h1>
+      .then((data) => {
+        setLoading(false);
+        setProject(data);
+      })
+      .catch((e) => {
+        setLoading(false);
+        setError(e.message);
+      });
+  }, [id]);
 
-        {loading && (
-          <div className="center-page">
-            <span className="spinner primary"></span>
-            <p>Loading...</p>
+  return (
+    <React.Fragment>
+      <h1>Project Detail</h1>
+
+      {loading && (
+        <div className="center-page">
+          <span className="spinner primary"></span>
+          <p>Loading...</p>
+        </div>
+      )}
+
+      {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse "></span> {error}
+              </p>
+            </section>
           </div>
-        )}
+        </div>
+      )}
 
-        {error && (
-          <div className="row">
-            <div className="card large error">
-              <section>
-                <p>
-                  <span className="icon-alert inverse "></span> {error}
-                </p>
-              </section>
-            </div>
-          </div>
-        )}
-
-        {project && <ProjectDetail project={project} />}
-      </React.Fragment>
-    );
-  }
-}
+      {project && <ProjectDetail project={project} />}
+    </React.Fragment>
+  );
+};
 
 export default ProjectPage;
+
+// interface ProjectPageState {
+//   loading: boolean;
+//   project: Project | undefined;
+//   error: string | undefined;
+// }
+
+// class ProjectPage extends React.Component<any, ProjectPageState> {
+//   state = {
+//     loading: false,
+//     project: undefined,
+//     error: ''
+//   };
+
+//   componentDidMount() {
+//     const id = Number(this.props.match.params.id);
+//     this.setState({ loading: true });
+//     projectAPI
+//       .find(id)
+//       .then(data => this.setState({ project: data, loading: false }))
+//       .catch(e => this.setState({ error: e.message, loading: false }));
+//   }
+//   render() {
+//     const { loading, project, error } = this.state;
+//     return (
+//       <React.Fragment>
+//         <h1>Project Detail</h1>
+
+//         {loading && (
+//           <div className="center-page">
+//             <span className="spinner primary"></span>
+//             <p>Loading...</p>
+//           </div>
+//         )}
+
+//         {error && (
+//           <div className="row">
+//             <div className="card large error">
+//               <section>
+//                 <p>
+//                   <span className="icon-alert inverse "></span> {error}
+//                 </p>
+//               </section>
+//             </div>
+//           </div>
+//         )}
+
+//         {project && <ProjectDetail project={project} />}
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// export default ProjectPage;
