ec8d19695c71999fdddecdf22204f98151ab0dfb
Craig McKeachie <craigdmckeachie@gmail.com>
2020-06-23 15:54:53 -0400

    page to fc

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -2,15 +2,13 @@ import React, { Fragment } from 'react';
 import { MOCK_PROJECTS } from './MockProjects';
 import ProjectList from './ProjectList';
 
-class ProjectsPage extends React.Component {
-  render() {
-    return (
-      <Fragment>
-        <h1>Projects</h1>
-        <ProjectList projects={MOCK_PROJECTS} />
-      </Fragment>
-    );
-  }
+function ProjectsPage() {
+  return (
+    <Fragment>
+      <h1>Projects</h1>
+      <ProjectList projects={MOCK_PROJECTS} />
+    </Fragment>
+  );
 }
 
 export default ProjectsPage;
