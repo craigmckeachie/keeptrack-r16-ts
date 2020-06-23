93d19703aa50639207a6e21c37ed1ae03e6bb8bb
Craig McKeachie <craigdmckeachie@gmail.com>
2020-06-23 14:33:29 -0400

    convert page to function component

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,15 +1,13 @@
 import React, { Fragment } from 'react';
 import { MOCK_PROJECTS } from './MockProjects';
 
-class ProjectsPage extends React.Component {
-  render() {
-    return (
-      <Fragment>
-        <h1>Projects</h1>
-        <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
-      </Fragment>
-    );
-  }
+function ProjectsPage() {
+  return (
+    <Fragment>
+      <h1>Projects</h1>
+      <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+    </Fragment>
+  );
 }
 
 export default ProjectsPage;
