dd1f5ad81db2971506e6bfdc316d2901e6bd4b3b
Craig McKeachie <craigdmckeachie@gmail.com>
2020-06-23 14:23:47 -0400

    update page to be function component

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-class ProjectsPage extends React.Component {
-  render(){
-    return <h1>Projects</h1>;
-  }
+function ProjectsPage() {
+  return <h1>Projects</h1>;
 }
 
 export default ProjectsPage;
