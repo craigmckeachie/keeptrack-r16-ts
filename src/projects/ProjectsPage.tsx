248055d66f852268ee3e16cb290082aa86a7cf0d
Craig McKeachie <craigdmckeachie@gmail.com>
2020-06-23 16:27:37 -0400

    page to fc

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -3,18 +3,17 @@ import { MOCK_PROJECTS } from './MockProjects';
 import ProjectList from './ProjectList';
 import { Project } from './Project';
 
-class ProjectsPage extends React.Component {
-  saveProject = (project: Project) => {
+function ProjectsPage() {
+  const saveProject = (project: Project) => {
     console.log('Saving project: ', project);
   };
-  render() {
-    return (
-      <Fragment>
-        <h1>Projects</h1>
-        <ProjectList onSave={this.saveProject} projects={MOCK_PROJECTS} />
-      </Fragment>
-    );
-  }
+
+  return (
+    <Fragment>
+      <h1>Projects</h1>
+      <ProjectList onSave={saveProject} projects={MOCK_PROJECTS} />
+    </Fragment>
+  );
 }
 
 export default ProjectsPage;
