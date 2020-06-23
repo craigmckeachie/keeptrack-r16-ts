b3e2fb2bdb8684393cddf04325891a9d95efea24
Craig McKeachie <craigdmckeachie@gmail.com>
2020-06-23 16:52:11 -0400

    page to fc

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,32 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { MOCK_PROJECTS } from './MockProjects';
 import ProjectList from './ProjectList';
 import { Project } from './Project';
 
-interface ProjectsPageState {
-  projects: Project[];
-}
+function ProjectsPage() {
+  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
 
-class ProjectsPage extends React.Component<any, ProjectsPageState> {
-  state = {
-    projects: MOCK_PROJECTS
-  };
-  saveProject = (project: Project) => {
-    this.setState((previousState: ProjectsPageState) => {
-      let projects = previousState.projects.map((p: Project) => {
-        return p.id === project.id ? project : p;
-      });
-      return { projects };
+  const saveProject = (project: Project) => {
+    // console.log('Saving project: ', project);
+    let updatedProjects = projects.map((p: Project) => {
+      return p.id === project.id ? project : p;
     });
+    setProjects(updatedProjects);
   };
-  render() {
-    return (
-      <Fragment>
-        <h1>Projects</h1>
-        <ProjectList onSave={this.saveProject} projects={this.state.projects} />
-      </Fragment>
-    );
-  }
+
+  return (
+    <Fragment>
+      <h1>Projects</h1>
+      {/* <ProjectList onSave={saveProject} projects={MOCK_PROJECTS} /> */}
+      <ProjectList onSave={saveProject} projects={projects} />
+    </Fragment>
+  );
 }
 
 export default ProjectsPage;
