acf1b3fe689b3e05355f35342f21a3bd7f7bb8c9
Craig McKeachie <craigdmckeachie@gmail.com>
2020-07-31 13:37:58 -0400

    page to func component and use React Redux hooks

---
+++ src/projects/ProjectsPage-connect.tsx
@@ -0,0 +1,85 @@
+import React, { Fragment } from 'react';
+import ProjectList from './ProjectList';
+import { Project } from './Project';
+import { AppState } from '../state';
+import { ProjectState } from './state/projectTypes';
+import { loadProjects, saveProject } from './state/projectActions';
+import { connect } from 'react-redux';
+
+class ProjectsPage1 extends React.Component<any> {
+  loadProjects(page: number) {
+    this.props.onLoad(page);
+  }
+
+  componentDidMount() {
+    this.loadProjects(1);
+  }
+
+  handleMoreClick = () => {
+    const nextPage = this.props.page + 1;
+    this.loadProjects(nextPage);
+  };
+
+  saveProject = (project: Project) => {
+    this.props.onSave(project);
+  };
+  render() {
+    return (
+      <Fragment>
+        <h1>Projects</h1>
+        {this.props.error && (
+          <div className="row">
+            <div className="card large error">
+              <section>
+                <p>
+                  <span className="icon-alert inverse "></span>
+                  {this.props.error}
+                </p>
+              </section>
+            </div>
+          </div>
+        )}
+
+        <ProjectList onSave={this.saveProject} projects={this.props.projects} />
+
+        {!this.props.loading && !this.props.error && (
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="button-group fluid">
+                <button
+                  className="button default"
+                  onClick={this.handleMoreClick}
+                >
+                  More...
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {this.props.loading && (
+          <div className="center-page">
+            <span className="spinner primary"></span>
+            <p>Loading...</p>
+          </div>
+        )}
+      </Fragment>
+    );
+  }
+}
+
+// export default ProjectsPage;
+
+// React Redux (connect)---------------
+function mapStateToProps(state: AppState): ProjectState {
+  return {
+    ...state.projectState,
+  };
+}
+
+const mapDispatchToProps = {
+  onLoad: loadProjects,
+  onSave: saveProject,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectsPage1);
--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,88 +1,74 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import ProjectList from './ProjectList';
 import { Project } from './Project';
+import { useSelector, useDispatch } from 'react-redux';
 import { AppState } from '../state';
-import { ProjectState } from './state/projectTypes';
-import { loadProjects, saveProject } from './state/projectActions';
-import { connect } from 'react-redux';
+import { loadProjects } from './state/projectActions';
 
-class ProjectsPage extends React.Component<any> {
-  loadProjects(page: number) {
-    this.props.onLoad(page);
-  }
+function ProjectsPage() {
+  const loading = useSelector(
+    (appState: AppState) => appState.projectState.loading
+  );
+  const projects = useSelector(
+    (appState: AppState) => appState.projectState.projects
+  );
+  const error = useSelector(
+    (appState: AppState) => appState.projectState.error
+  );
+  const currentPage = useSelector(
+    (appState: AppState) => appState.projectState.page
+  );
+  const dispatch = useDispatch();
 
-  componentDidMount() {
-    this.loadProjects(1);
-  }
+  useEffect(() => {
+    dispatch(loadProjects(1));
+  }, [dispatch]);
 
-  handleMoreClick = () => {
-    const nextPage = this.props.page + 1;
-    this.loadProjects(nextPage);
+  const handleMoreClick = () => {
+    dispatch(loadProjects(currentPage + 1));
   };
 
-  saveProject = (project: Project) => {
-    this.props.onSave(project);
+  const saveProject = (project: Project) => {
+    dispatch(saveProject(project));
   };
-  render() {
-    return (
-      <Fragment>
-        <h1>Projects</h1>
-        {this.props.error && (
-          <div className="row">
-            <div className="card large error">
-              <section>
-                <p>
-                  <span className="icon-alert inverse "></span>
-                  {this.props.error}
-                </p>
-              </section>
+
+  return (
+    <Fragment>
+      <h1>Projects</h1>
+      {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse "></span>
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
+      <ProjectList onSave={saveProject} projects={projects} />
+
+      {!loading && !error && (
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="button-group fluid">
+              <button className="button default" onClick={handleMoreClick}>
+                More...
+              </button>
             </div>
           </div>
-        )}
+        </div>
+      )}
 
-        <ProjectList onSave={this.saveProject} projects={this.props.projects} />
-
-        {!this.props.loading && !this.props.error && (
-          <div className="row">
-            <div className="col-sm-12">
-              <div className="button-group fluid">
-                <button
-                  className="button default"
-                  onClick={this.handleMoreClick}
-                >
-                  More...
-                </button>
-              </div>
-            </div>
-          </div>
-        )}
-
-        {this.props.loading && (
-          <div className="center-page">
-            <span className="spinner primary"></span>
-            <p>Loading...</p>
-          </div>
-        )}
-      </Fragment>
-    );
-  }
+      {loading && (
+        <div className="center-page">
+          <span className="spinner primary"></span>
+          <p>Loading...</p>
+        </div>
+      )}
+    </Fragment>
+  );
 }
 
-// export default ProjectsPage;
-
-// React Redux (connect)---------------
-function mapStateToProps(state: AppState): ProjectState {
-  return {
-    ...state.projectState
-  };
-}
-
-const mapDispatchToProps = {
-  onLoad: loadProjects,
-  onSave: saveProject
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ProjectsPage);
+export default ProjectsPage;
